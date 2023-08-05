2c20a9926caa154078ee2c8415387e6ef5c55555
Phil Hasenkamp <89226935+beautyTruth@users.noreply.github.com>
2023-08-04 21:33:18 -0500

    complete lecture 116, installing the BeautifulSoup stuff

---
+++ lecture_116.py
@@ -0,0 +1,19 @@
+import math
+
+omelette = math.pi
+
+def whatamidoing(meat,potatoes):
+    print(f'I am happy and learning {meat} and {potatoes} walking')
+
+    print(meat+potatoes*omelette)
+
+    sunnysideup = meat+potatoes*omelette
+
+    return sunnysideup
+
+beefy = whatamidoing(7,77)
+
+print(beefy + beefy)
+
+
+    
